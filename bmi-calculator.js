7224fa487d503748603b8b2f5137508e2b3ff8ce
Mohd Rihan <137299852+dfuwhef234@users.noreply.github.com>
2024-09-15 15:56:53 +0530

    Update bmi-calculator.js

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -1,25 +1,3 @@
-// const form  = document.querySelector('form');
-
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-
-//     const height = parseInt(document.querySelector('#height').value);
-//     const weight = parseInt(document.querySelector('#weight').value);
-//     const results = document.querySelector('#results');
-
-//     if(height === '' || height < 0 || isNaN(height)){
-//         results.innerHTML = `enter the  valid height ${height}`;
-
-//     }else if(weight === '' || weight < 0 || isNaN(weight)){
-//         results.innerHTML = `enter the  valid weight ${weight}`;
-//     }else{
-//         const bmi = (weight / ((height*height)/10000)).toFixed(2);
-//         results.innerHTML = `<span>${bmi}</span>`;
-//     }
-
-// });
-
-
 
 const form = document.querySelector('form');
 
